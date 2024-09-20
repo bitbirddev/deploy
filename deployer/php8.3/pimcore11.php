a6c3d2eeca56e40f144bb33f541d8c8c8d65debb
bitbird development <bs@bitbird.de>
2024-09-20 11:14:09 +0200

    adding configs for php8.3

---
+++ deployer/php8.3/pimcore11.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Deployer;
+
+########## REQUIRE PACKAGES ############################################
+require 'contrib/rsync.php';
+require 'recipe/pimcore.php';
+require 'contrib/php-fpm.php';
+
+########## SET CONFIG OPTION ###########################################
+set('bin/php', 'php8.3');
+set('php_fpm_version', '8.3');
+set('keep_releases', 2);
+set('writable_dirs', ['var', 'var/cache', 'var/cache/dev', 'var/cache/prod', 'var/log', 'var/sessions', 'public/var']);
+set('ssh_multiplexing', true);
+add('rsync', [ 'exclude' => [ '.git', 'node_modules', '.github', 'deploy.php', ], ]);
+
+
+########## TASKS #######################################################
+task('composer:post-update-cmd', function () {
+    run('cd {{release_or_current_path}} && {{bin/composer}} run-script post-update-cmd 2>&1');
+})->desc('clears cache, executes Migrations if any and installs Symlinks for Bundles');
+
+task('pimcore:cache:clear', function () {
+    run('{{bin/console}} pimcore:cache:clear');
+})->desc("Clear Pimcore Cache");
+
+task('symfony:cache:clear', function () {
+    run('{{bin/console}} cache:clear');
+})->desc("Clear Symfony Cache");
+
+task('symfony:stop-workers', function () {
+    run('{{bin/console}} messenger:stop-workers');
+})->desc('Restarting Queue Workers');
+
+########## HOOKS #######################################################
+after('deploy:failed', 'deploy:unlock');
+after('deploy:success', 'php-fpm:reload');
+
+########## HOOKS #######################################################
+task('deploy', [
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:writable',
+    'symfony:cache:clear',
+    'composer:post-update-cmd',
+    'pimcore:rebuild-classes',
+    'pimcore:cache:clear',
+    'symfony:stop-workers',
+    'deploy:publish',
+])->desc('Deploy the application');
